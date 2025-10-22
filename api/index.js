99a4f5f95962b6232ecfb59a3fb8e5252dbd5256
Jyle Vergara <jylevergara@gmail.com>
2025-10-22 18:49:31 -0400

    add debug code

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
-const app = require('../backend/server.js');
+const app = require('../backend/server');
 
-module.exports = app;
+module.exports = (req, res) => {
+  console.log(`${req.method} ${req.url}`);
+  
+  try {
+    return app(req, res);
+  } catch (error) {
+    console.error('Handler error:', error);
+    return res.status(500).json({ 
+      error: error.message,
+      type: error.constructor.name
+    });
+  }
+};
